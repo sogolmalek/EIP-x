c9e431683e4702ad2afa78f900dc98d38808481c
sogol <sogol.malek1@gmail.com>
2023-08-06 15:50:24 +0200

    Create light_client_protocol.rs

---
+++ Client-Rust/light_client_protocol.rs
@@ -0,0 +1,13 @@
+// light_client_protocol.rs
+
+use ethers::types::{Address, U256};
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum LightClientMessage {
+    RequestBalance(Address),
+    ResponseBalance(Address, U256),
+    // Add more message types as needed
+}
+
+// Implement any necessary helper functions or data structures: ...
