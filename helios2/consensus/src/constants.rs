356921c9b61c5bd969b0c2cfec4745d752279a04
samtvlabs <fatheroffire@proton.me>
2023-09-10 12:15:47 +0000

    chore: clean up

--- helios2/consensus/src/constants.rs
+++
@@ -1,4 +0,0 @@
-// Consensus constants
-
-// https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/p2p-interface.md#configuration
-pub const MAX_REQUEST_LIGHT_CLIENT_UPDATES: u8 = 128;
