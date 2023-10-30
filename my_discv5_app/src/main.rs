3e6a9d839a94fe6f701f9368d1b5ffef0982d523
sogol Malek <sogol.malek1@gmail.com>
2023-10-30 12:04:34 +0330

    implemented a loop that periodically sends FINDNODE requests to the connected nodes.adjusted the sleep duration to 10 milliseconds, you can use the tokio library, which provides more fine-grained control over asynchronous tasks and timers

--- my_discv5_app/src/main.rs
+++ my_discv5_app/src/main.rs
@@ -1,26 +1,22 @@
 use async_std::task;
+use async_std::stream::StreamExt;
 use discv5::{NodeDiscovery, NodeRecord, CombinedKey, CombinedKeyExt, Config, Discv5Error};
 use std::net::{SocketAddr, Ipv4Addr};
 use std::time::Duration;
+use rand::Rng;
 
 async fn send_findnode_request(
     node_discovery: &NodeDiscovery,
     node: &NodeRecord,
     zkp_message: &[u8],
 ) -> Result<(), Discv5Error> {
-    let request_id: [u8; 8] = rand::random(); // Generate a random request ID
-    let distances = vec![0, 1, 2]; // Logarithmic distances to query
+    let request_id: [u8; 8] = rand::random();
+    let distances = vec![0, 1, 2];
 
-    // Generate the FINDNODE Request message
     let request = node_discovery.findnode(request_id, distances)?;
-
-    // Extract the node's address and port
     let address = node.udp_socket().unwrap().local_addr().unwrap();
-
-    // Append the ZKP message to the request as payload
     let request_with_payload = [&request[..], zkp_message].concat();
 
-    // Send the request to the node
     send_udp_request(&address, &request_with_payload).await
 }
 
@@ -28,6 +24,34 @@ async fn send_udp_request(address: &SocketAddr, request: &[u8]) -> Result<(), Di
     let socket = async_std::net::UdpSocket::bind("0.0.0.0:0").await?;
     socket.send_to(request, address).await?;
     Ok(())
+}
+
+async fn periodic_findnode_requests(
+    node_discovery: &NodeDiscovery,
+    connected_nodes: Vec<NodeRecord>,
+    zkp_message: &[u8],
+) {
+    let mut interval = async_std::stream::interval(Duration::from_millis(10));
+
+    loop {
+        interval.next().await;
+
+        for node in &connected_nodes {
+            match send_findnode_request(node_discovery, node, zkp_message).await {
+                Ok(_) => {
+                    println!(
+                        "FINDNODE Request sent successfully to node: {:?}",
+                        node.udp_socket().unwrap().local_addr().unwrap()
+                    );
+                }
+                Err(err) => eprintln!(
+                    "Failed to send FINDNODE Request to node {:?}: {:?}",
+                    node.udp_socket().unwrap().local_addr().unwrap(),
+                    err
+                ),
+            }
+        }
+    }
 }
 
 fn main() {
@@ -40,7 +64,10 @@ fn main() {
             local_key,
             local_peer_id: local_enr.node_id(),
             listen_address: Some("0.0.0.0:9000".parse::<SocketAddr>().unwrap()),
-            bootnodes: vec![SocketAddr::new(Ipv4Addr::new(1, 2, 3, 4), 30303)], // Replace with actual bootnodes
+            bootnodes: vec![
+                // Replace with actual bootnodes
+                SocketAddr::new(Ipv4Addr::new(1, 2, 3, 4), 30303),
+            ],
             request_timeout: Duration::from_secs(5),
             max_request_retries: 3,
             ..Config::default()
@@ -65,6 +92,12 @@ fn main() {
             ),
             // Add more NodeRecords as needed
         ];
+
+        let _ = task::spawn(periodic_findnode_requests(
+            &node_discovery,
+            connected_nodes,
+            zkp_message,
+        ));
 
         for node in connected_nodes {
             match send_findnode_request(&node_discovery, &node, zkp_message).await {
